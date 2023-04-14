59a29cbe5ae820eeb31da62239009e4e62df67a3
Jish123K <128099655+Jish123K@users.noreply.github.com>
2023-04-14 20:13:39 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,233 @@
+import java.io.File;
+
+import java.io.IOException;
+
+import java.io.InputStream;
+
+import java.io.OutputStream;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+import java.util.Scanner;
+
+import org.opencv.core.Core;
+
+import org.opencv.core.Mat;
+
+import org.opencv.core.Point;
+
+import org.opencv.core.Scalar;
+
+import org.opencv.imgproc.Imgproc;
+
+import org.opencv.videoio.VideoCapture;
+
+import pytesseract.Tesseract;
+
+import torch.Tensor;
+
+import torch.nn.functional.softmax;
+public class CarDetector {
+
+    private static final String PLATE_MODEL_PATH = "/path/to/plate-model.pt";
+
+    private static final String COLOR_MODEL_PATH = "/path/to/color-model.pt";
+
+    private static final String STATE_MODEL_PATH = "/path/to/state-model.pt";
+
+    private static final String MAKE_MODEL_PATH = "/path/to/make-model.pt";
+
+    private static final int CAMERA_WIDTH = 640;
+
+    private static final int CAMERA_HEIGHT = 480;
+
+    private static final int PLATE_WIDTH = 300;
+
+    private static final int PLATE_HEIGHT = 100;
+
+    private static final int COLOR_THRESHOLD = 100;
+
+    private static final String CSV_FILE_PATH = "/path/to/csv-file.csv";
+
+    private VideoCapture camera;
+
+    private Tesseract tesseract;
+
+    private YOLOv5 plateDetector;
+
+    private FastCNN colorDetector;
+
+    private StatePredictor statePredictor;
+
+    private MakeModelPredictor makeModelPredictor;
+
+    private List<Car> cars;
+
+    public CarDetector() throws IOException {
+
+        camera = new VideoCapture(0);
+
+        tesseract = new Tesseract();
+
+        plateDetector = new YOLOv5(PLATE_MODEL_PATH);
+
+        colorDetector = new FastCNN(COLOR_MODEL_PATH);
+
+        statePredictor = new StatePredictor(STATE_MODEL_PATH);
+
+        makeModelPredictor = new MakeModelPredictor(MAKE_MODEL_PATH);
+
+        cars = new ArrayList<>();
+
+    }
+
+    public void start() {
+
+        while (true) {
+
+            Mat frame = new Mat();
+
+            if (!camera.read(frame)) {
+
+                break;
+
+            }
+            / Detect plates
+
+            List<Mat> plateBoxes = plateDetector.detect(frame);
+
+            // For each plate box, extract the plate number and color
+
+            for (Mat plateBox : plateBoxes) {
+
+                String plateNumber = tesseract.doOCR(frame, plateBox);
+
+                int color = colorDetector.predict(frame, plateBox);
+
+                // Create a new Car object
+
+                Car car = new Car();
+
+                car.setPlateNumber(plateNumber);
+
+                car.setColor(color);
+
+                car.setTime(System.currentTimeMillis());
+
+                // Add the car to the list of cars
+
+                cars.add(car);
+
+            }
+
+            // Write the cars to the CSV file
+
+            try (OutputStream outputStream = new FileOutputStream(CSV_FILE_PATH)) {
+
+                CSVUtils.write(outputStream, cars);
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+            // Upload the data to Google Cloud Storage
+
+            try {
+
+                GoogleCloudStorageUtils.upload(CSV_FILE_PATH, "gs://my-bucket/car-data.csv");
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+
+        CarDetector detector = new CarDetector();
+
+        detector.start();
+
+    }
+
+}
+
+class CSVUtils {
+
+    public static void write(OutputStream outputStream, List<Car> cars) throws IOException {
+
+        // Create a CSV writer
+
+        CSVWriter writer = new CSVWriter(outputStream);
+
+        // Write the header row
+
+        writer.writeNext(new String[]{"plateNumber", "color", "time", "state", "make", "model"});
+
+        // Write the data rows
+
+        for (Car car : cars) {
+
+            writer.writeNext(new String[]{car.getPlateNumber(), car.getColor(), car.getTime(), car.getState(), car.getMake(), car.getModel()});
+
+        }
+
+        // Close the writer
+
+        writer.close();
+
+    }
+
+}
+
+class GoogleCloudStorageUtils {
+
+    public static void upload(String filePath, String bucketName) throws IOException {
+
+        // Create a Google Cloud Storage client
+
+        Storage storage = new Storage();
+
+        // Create a bucket
+
+        Bucket bucket = storage.create(bucketName);
+
+        // Upload the file
+
+        Blob blob = bucket.create(filePath);
+
+    }
+
+}
+public static void upload(String filePath, String bucketName) throws IOException {
+
+        // Create a Google Cloud Storage client
+
+        Storage storage = new Storage();
+
+        // Create a bucket
+
+        Bucket bucket = storage.create(bucketName);
+
+        // Upload the file
+
+        Blob blob = bucket.create(filePath);
+
+        // Get the blob's URL
+
+        String url = blob.getUrl();
+
+        // Print the URL
+
+        System.out.println("The file has been uploaded to the following URL: " + url);
+
+    }
+
+}
